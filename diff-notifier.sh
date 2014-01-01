42fb602b80cbac1c29c5e05331b7c87f6f2108a8
Veevee Cai <veevee@masterstreet.com>
2014-01-01 11:33:14 -0700

    Added diff variation

---
+++ diff-notifier.sh
@@ -0,0 +1,49 @@
+DIFF_FOLDER=./old_diffs
+
+updated=""
+
+
+
+
+echo
+while read line
+do
+    author=`echo $line | awk -F';' '{print $1}'`
+    url=`echo $line | awk -F';' '{print $2}'`
+
+    echo -e "Checking" $author...
+    echo $url
+
+    old="$DIFF_FOLDER/$author.html"
+    new="$DIFF_FOLDER/$author.tmp"
+
+    # Initialize new webpage trackers
+    if [ ! -f $old ]
+    then
+        curl -s "$url" > "$old"
+        echo Initialized webpage.
+    else
+        s=`echo $url | tr -d " "`
+        #curl $s
+        curl -s "$url" > "$new"
+        difflines=`diff "$old" "$new" | wc -l`
+
+        if [ ! $difflines = 0 ]
+        then
+            echo Updates detected!
+            mv "$new" "$old"
+            updated="$updated$author: $url\n"
+        else
+            rm $new
+            echo No updates.
+        fi
+    fi
+    echo
+done < <(cat diff_urls.txt)
+
+if [ -n "$updated" ]
+then
+    echo -e "Changes were discovered!\n"
+    echo -e "Sending email to $(whoami):" $updated "\n"
+    echo -e $updated | mail -s "Updated websites $(date "+%b %d %Y")" `whoami`@`hostname`
+fi
