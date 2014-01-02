013a9520219741819e5962d6c58f59303dbbf941
Levi Cai <cail@seas.upenn.edu>
2014-01-02 15:00:16 -0700

    Fixed bug where different sized pages caused script to crash

--- img-notifier.sh
+++ img-notifier.sh
@@ -3,6 +3,29 @@ LOG=$DIFF_FOLDER/logs.txt
 
 updated=""
 
+is_same() {
+    # Returns 1 if they are the same image, 0 otherwise
+    old_size=`identify $1 | awk '{print $3}'`
+    new_size=`identify $2 | awk '{print $3}'`
+
+    if [ ! $old_size = $new_size ]
+    then
+	echo 0
+	return 1
+    fi
+
+    diff_pixels=$((compare -metric ae $1 $2 $1.compare) 2>&1)
+    rm $1.compare
+
+    if [ ! $diff_pixels = 0 ]
+    then
+	echo 0
+	return 1
+    fi
+    echo 1
+    return 1
+}
+
 echo
 while read line
 do
@@ -18,14 +41,18 @@ do
     # Initialize new webpage trackers
     if [ ! -f $old ]
     then
-        webkit2png -F -o $author -D $DIFF_FOLDER $url > $LOG
+        webkit2png -F -o $author -D $DIFF_FOLDER $url > $LOG 2>&1
         echo Initialized webpage.
     else
-	webkit2png -F -o $author-tmp -D $DIFF_FOLDER $url > $LOG
-        diffpixels=`compare -metric ae $old $new $old.compare`
-	rm $old.compare
+	webkit2png -F -o $author-tmp -D $DIFF_FOLDER $url > $LOG 2>&1
 
-        if [ ! $diffpixels = 0 ]
+	same=$(is_same $old $new)
+
+#        diffpixels=$((compare -metric ae $old $new $old.compare) 2>&1)
+#	echo "$diffpixels pixels changed"
+#	rm $old.compare
+
+        if [ "$same" = 0 ]
         then
             echo Updates detected!
             mv "$new" "$old"
@@ -40,7 +67,10 @@ done < <(cat diff_urls.txt)
 
 if [ -n "$updated" ]
 then
-    echo -e "Changes were discovered!\n"
-    echo -e "Sending email to $(whoami):" $updated "\n"
-    echo -e "--------------------\n"$updated"\n----------------------" | mail -s "Updated websites $(date "+%b %d %Y")" `whoami`@`hostname`
+    echo -e "The following pages had updates:\n"
+    echo -e $updated
+    #echo -e "Sending email to $(whoami):" $updated "\n"
+    #echo -e "--------------------\n"$updated"\n----------------------" | mail -s "Updated websites $(date "+%b %d %Y")" `whoami`@`hostname`
+else
+    echo "No updated pages were found."
 fi
